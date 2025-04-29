a5c1659339c4704e29ab58b834b4a102a78c0975
minho2618 <minho2618@naver.com>
2025-04-29 10:48:54 +0900

    [fix] PublishDate -> MyDate 수정

---
+++ src/com/self/service/BookManager.java
@@ -0,0 +1,20 @@
+package com.self.service;
+
+import java.util.ArrayList;
+
+import com.self.vo.Book;
+
+public interface BookManager {
+	void insertBook(Book book);
+	void deleteBook(int isbn);
+	void updateBook(Book book);
+	Book getBook(int isbn);
+	ArrayList<Book> getAllBook();
+	int getNumberOfBooks();
+	ArrayList<Book> searchBookByTitle(String title);
+	ArrayList<Book> searchBookByPrice(int min, int max);
+	double getSumPriceOfBooks();
+	double getAvgPriceOfBooks();	
+	
+	ArrayList<Book> magazineOfThisYearInfo();
+}
